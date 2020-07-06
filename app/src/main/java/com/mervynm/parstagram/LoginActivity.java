f07f183f9e4fb0de2be52b3ed004f9944f7a04e4
Mervyn Mathew <mmervyn@fb.com>
2020-07-06 17:40:01 -0400

    User is now able to log in using Parse

--- app/src/main/java/com/mervynm/parstagram/LoginActivity.java
+++ app/src/main/java/com/mervynm/parstagram/LoginActivity.java
@@ -2,11 +2,17 @@ package com.mervynm.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -36,5 +42,21 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "loginUser: Attempting to login user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login in", e);
+                    return;
+                }
+                goToMainActivity();
+                Toast.makeText(LoginActivity.this, "Successful Login", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
